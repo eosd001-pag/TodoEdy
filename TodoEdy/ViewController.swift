1cf60b8808434dd7c8a9ec94884ba200855a73a6
Eddie Ortega <eddieortega@eddies-imac.local>
2019-02-10 12:40:21 -0500

    Initial Commit

---
+++ TodoEdy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoEdy
+//
+//  Created by Eddie Ortega on 2/10/19.
+//  Copyright © 2019 Ortegalive Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
