3c0699b3868e901e343742078fd8d1eda0cf9de8
Eddie Ortega <eddieortega@eddies-imac.local>
2019-02-12 13:32:26 -0500

    Todo list complete with datasource and delegate methods

---
+++ TodoEdy/TodoListViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  TodoEdy
+//
+//  Created by Eddie Ortega on 2/10/19.
+//  Copyright © 2019 Ortegalive Software. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Finde Mike", "Buy Eggs", "Destroy Dragons"]
+    // Declare intance variables here
+    
+    // Declare linked IBOulets
+    @IBOutlet var todoTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK - Tableview Datasource Methods
+    
+    //TODO: Declare numberOfRowsInSection here:
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    //MARK - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //print(itemArray[indexPath.row])
+        
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        
+    }
+    
+
+}
+
